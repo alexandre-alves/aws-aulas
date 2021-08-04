ee8127a281acf4c1a05f66e02a8d79307b449e26
alexandre <alexandre.alvesm@gmail.com>
2021-08-04 20:04:25 -0300

    Initial commit

--- main.tf
+++ main.tf
@@ -1,25 +1,15 @@
-terraform {
-  required_version = "0.14.4"
-  ##OKppdzdwssds
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.23.0"
-    }
-  }
-}
-
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 provider "aws" {
-  region  = "eu-central-1" # Brasil -> us-east-1
-  profile = "tf014"
+  region  = "us-east-1" # Brasil -> us-east-1
+  profile = "t3rr@"
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
+#Esta sendo criado um recurso S3 Bucket esse recurso dentro da estrutura do Terraforme sera usado o nome my-teste-bucket
 resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "my-tf-test-bucket-123123455745642342342"
+  bucket = "my-tf-test-bucket-xptobolinha322"
   acl    = "private"
-
+##
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
