f70e79198f8d6ae7a222f58ab57714b1ee686a51
alexandre <alexandre.alvesm@gmail.com>
2021-08-16 13:46:10 -0300

    Update README.md

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region  = "us-east-1" # Brasil -> us-east-1
   profile = "t3rr@"
 }
-
+#########
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
 #Esta sendo criado um recurso S3 Bucket esse recurso dentro da estrutura do Terraforme sera usado o nome my-teste-bucket
 resource "aws_s3_bucket" "my-test-bucket" {
