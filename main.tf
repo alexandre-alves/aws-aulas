9e0c6f87810e97c5465c746f912475623c7666bd
alexandre <alexandre.alvesm@gmail.com>
2021-08-23 14:52:33 -0300

    COmit

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 provider "aws" {
-  region  = "us-east-1" # Brasil -> us-east-1
-  profile = "t3rr@"
+#Uso sem  Variavel
+  #  region  = "us-east-1" # Brasil -> us-east-1
+  region = var.aws_region
+  #profile = "t3rr@"
+  profile = var.aws_profile
+
 }
 ##
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
@@ -9,12 +13,20 @@ provider "aws" {
 resource "aws_s3_bucket" "my-test-bucket" {
   bucket = "my-tf-test-bucket-xptobolinha322"
   acl    = "private"
-##
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-    ManagedBy   = "Terraform"
-    Owner = "Alexandre"
-    UpdateAt = "2021-08-23"
+  ##
+  tags = var.instance_tags
+#  tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#    ManagedBy   = "Terraform"
+#   Owner       = "Alexandre"
+#   UpdateAt    = "2021-08-23"
+  }
+  resource "aws_instance" "web" {
+    ami           = var.instance_ami
+    instance_type = var.instance_type #"t2.micro"
+
+    tags = {
+      Name = "HelloWorld"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,28 @@
-#
-####
+variable "aws_region" {
+  type = string
+  description = ""
+  default = "us-eest-1"
+}
+variable "aws_profile" {
+  type = string
+  description = ""
+  default = "t3rra"
+}
+variable "instance_ami" {
+  type = string
+  description = ""
+  default = "ami-0ee02acd56a52998e"
+}
+variable "instance_type" {
+  type = string
+  description = ""
+  default = "t2.micro"
+}
+variable "instance_tags" {
+  type = map(string)
+  description = ""
+  default = {
+    Name = "Ubuntu16"
+    Project = "Curso Tf"
+  }
+}
