fcb4c0034d7f5ae27c065217e8cf16b21a1962cf
alexandre <alexandre.alvesm@gmail.com>
2021-08-23 18:00:41 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
-#Esta sendo criado um recurso S3 Bucket esse recurso dentro da estrutura do Terraforme sera usado o nome my-teste-bucket
+#Esta sendo criado um recurso S3 Bucket esse recurso dentro da estrutura do
+#Terraforme sera usado o nome my-teste-bucket
 resource "aws_s3_bucket" "my-test-bucket" {
   bucket = "my-tf-test-bucket-xptobolinha322"
   acl    = "private"
